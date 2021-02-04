3db75e3d32f93a41ddbea6e288c5cc46366fb02c
ukmumair <umairm60@gmail.com>
2021-02-04 12:23:59 +0530

    Replaced FindViewById With ViewBinding

--- app/src/main/java/com/unvnews/unvnews/CategoryPage.java
+++ app/src/main/java/com/unvnews/unvnews/CategoryPage.java
@@ -3,16 +3,11 @@ package com.unvnews.unvnews;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.appbar.MaterialToolbar;
 import com.unvnews.unvnews.databinding.ActivityCategoryPageBinding;
 
 import org.jetbrains.annotations.NotNull;
@@ -32,6 +27,7 @@ public class CategoryPage extends AppCompatActivity {
     MyAdapter adapter;
     List<Articles> articles;
     Models models = new Models();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,20 +38,15 @@ public class CategoryPage extends AppCompatActivity {
         articles = new ArrayList<>();
         binding.categoryProgressBar.setVisibility(View.VISIBLE);
         binding.materialToolbarCategory.setTitle(title);
-        binding.materialToolbarCategory.setNavigationOnClickListener(v -> {
-            finish();
-        });
+        binding.materialToolbarCategory.setNavigationOnClickListener(v -> finish());
         binding.materialToolbarCategory.setOnMenuItemClickListener(item -> {
-            Intent intent = null;
-           if (item.getItemId() == R.id.toolbarAbout)
-           {
-               intent = new Intent(CategoryPage.this,AboutActivity.class);
-               Toast.makeText(CategoryPage.this, "Hello", Toast.LENGTH_SHORT).show();
-           }
-           else
-           {
-               intent = new Intent(CategoryPage.this,SearchActivity.class);
-           }
+            Intent intent = new Intent();
+            if (item.getItemId() == R.id.toolbarAbout) {
+                intent.setClass(CategoryPage.this, AboutActivity.class);
+                Toast.makeText(CategoryPage.this, "Hello", Toast.LENGTH_SHORT).show();
+            } else {
+                intent.setClass(CategoryPage.this, SearchActivity.class);
+            }
             startActivity(intent);
             return true;
         });
@@ -66,30 +57,36 @@ public class CategoryPage extends AppCompatActivity {
 
         ApiInterface apiInterface = retrofit.create(ApiInterface.class);
 
-        Call<News> call ;
+        Call<News> call;
 
-        switch (title)
-        {
-            case "Top Headlines" : call = apiInterface.getArticle(models.getCOUNTRY(),models.getAPI_KEY());
-            break;
+        switch (title) {
+            case "Top Headlines":
+                call = apiInterface.getArticle(models.getCOUNTRY(), models.getAPI_KEY());
+                break;
 
-            case "Sports" : call = apiInterface.getArticleByCategory(models.getCOUNTRY(),"sports",models.getAPI_KEY());
-            break;
+            case "Sports":
+                call = apiInterface.getArticleByCategory(models.getCOUNTRY(), "sports", models.getAPI_KEY());
+                break;
 
-            case "Entertainment" : call = apiInterface.getArticleByCategory(models.getCOUNTRY(),"entertainment",models.getAPI_KEY());
-            break;
+            case "Entertainment":
+                call = apiInterface.getArticleByCategory(models.getCOUNTRY(), "entertainment", models.getAPI_KEY());
+                break;
 
-            case "Technology" : call = apiInterface.getArticleByCategory(models.getCOUNTRY(),"technology",models.getAPI_KEY());
-            break;
+            case "Technology":
+                call = apiInterface.getArticleByCategory(models.getCOUNTRY(), "technology", models.getAPI_KEY());
+                break;
 
-            case "Health" : call = apiInterface.getArticleByCategory(models.getCOUNTRY(),"health",models.getAPI_KEY());
-            break;
+            case "Health":
+                call = apiInterface.getArticleByCategory(models.getCOUNTRY(), "health", models.getAPI_KEY());
+                break;
 
-            case "Business" : call = apiInterface.getArticleByCategory(models.getCOUNTRY(),"business",models.getAPI_KEY());
-            break;
+            case "Business":
+                call = apiInterface.getArticleByCategory(models.getCOUNTRY(), "business", models.getAPI_KEY());
+                break;
 
-            case "Science" : call = apiInterface.getArticleByCategory(models.getCOUNTRY(),"science",models.getAPI_KEY());
-            break;
+            case "Science":
+                call = apiInterface.getArticleByCategory(models.getCOUNTRY(), "science", models.getAPI_KEY());
+                break;
 
             default:
                 throw new IllegalStateException("Unexpected value: " + title);
@@ -101,7 +98,7 @@ public class CategoryPage extends AppCompatActivity {
                 if (response.body() != null) {
                     articles = response.body().getArticles();
                 }
-                adapter = new MyAdapter(CategoryPage.this,articles);
+                adapter = new MyAdapter(CategoryPage.this, articles);
                 binding.categoryRecView.setAdapter(adapter);
                 binding.categoryProgressBar.setVisibility(View.INVISIBLE);
                 adapter.notifyDataSetChanged();
